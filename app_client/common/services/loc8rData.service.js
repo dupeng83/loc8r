9f9962cd57a9f4328dadd1da8129a892935948a2
dupeng <dupeng83@outlook.com>
2017-01-28 17:31:59 +0800

    10.3.3 增加了location详情页面

--- app_client/common/services/loc8rData.service.js
+++ app_client/common/services/loc8rData.service.js
@@ -13,8 +13,19 @@
       return $http.get('/api/locations?lng=126.62&lat=45.75&maxDistance=20');
       // return $http.get('/api/locations?lng=' + lng + '&lat=' + lat + '&maxDistance=20');
     };
+
+    var locationById = function (locationid) {
+      return $http.get('/api/locations/' + locationid);
+    };
+
+    // var addReviewById = function (locationid, data) {
+    //   return $http.post('/api/locations/' + locationid + '/reviews', data);
+    // };
+
     return {
-      locationByCoords : locationByCoords
+      locationByCoords : locationByCoords,
+      locationById : locationById,
+      // addReviewById : addReviewById
     };
   }
 
