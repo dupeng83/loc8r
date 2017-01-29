791848721628810dbfdb8e46b403ec6c96c95232
dupeng <dupeng83@outlook.com>
2017-01-29 19:28:16 +0800

    停止使用UgilifyJS创建文件, 为部署做准备

--- app.js
+++ app.js
@@ -18,30 +18,30 @@ var app = express();
 // app.set('views', path.join(__dirname, 'app_server', 'views'));
 // app.set('view engine', 'jade');
 
-var appClientFiles = [
-  'app_client/app.js',
-  'app_client/home/home.controller.js',
-  'app_client/about/about.controller.js',
-  'app_client/locationDetail/locationDetail.controller.js',
-  'app_client/reviewModal/reviewModal.controller.js',
-  'app_client/common/services/geolocation.service.js',
-  'app_client/common/services/loc8rData.service.js',
-  'app_client/common/filters/formatDistance.filter.js',
-  'app_client/common/filters/addHtmlLineBreaks.filter.js',
-  'app_client/common/directives/navigation/navigation.directive.js',
-  'app_client/common/directives/footerGeneric/footerGeneric.directive.js',
-  'app_client/common/directives/pageHeader/pageHeader.directive.js',
-  'app_client/common/directives/ratingStars/ratingStars.directive.js'
-];
-var uglified = uglifyJs.minify(appClientFiles, { compress : false });
+// var appClientFiles = [
+//   'app_client/app.js',
+//   'app_client/home/home.controller.js',
+//   'app_client/about/about.controller.js',
+//   'app_client/locationDetail/locationDetail.controller.js',
+//   'app_client/reviewModal/reviewModal.controller.js',
+//   'app_client/common/services/geolocation.service.js',
+//   'app_client/common/services/loc8rData.service.js',
+//   'app_client/common/filters/formatDistance.filter.js',
+//   'app_client/common/filters/addHtmlLineBreaks.filter.js',
+//   'app_client/common/directives/navigation/navigation.directive.js',
+//   'app_client/common/directives/footerGeneric/footerGeneric.directive.js',
+//   'app_client/common/directives/pageHeader/pageHeader.directive.js',
+//   'app_client/common/directives/ratingStars/ratingStars.directive.js'
+// ];
+// var uglified = uglifyJs.minify(appClientFiles, { compress : false });
 
-fs.writeFile('public/angular/loc8r.min.js', uglified.code, function (err){
-  if(err) {
-    console.log(err);
-  } else {
-    console.log("Script generated and saved:", 'loc8r.min.js');
-  }
-});
+// fs.writeFile('public/angular/loc8r.min.js', uglified.code, function (err){
+//   if(err) {
+//     console.log(err);
+//   } else {
+//     console.log("Script generated and saved:", 'loc8r.min.js');
+//   }
+// });
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
